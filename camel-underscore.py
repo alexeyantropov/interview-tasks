a8adc269bdade9ca0e575e5320f9664189367c85
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2022-10-28 19:00:06 +0300

    style(camel-underscore.py): comments and extra operands.

--- camel-underscore.py
+++ camel-underscore.py
@@ -5,26 +5,28 @@
 input_strings = ['this_is_the_text', 'thisIsTheText', 'thereIsA_mistake!']
 
 def cam_or_under (s: str) -> str:
+    # A var for collecting an output string.
     ret = str()
+    # A pointer initial value. It points to the start of the line.
     i = 0
+    # Two boolean variables for a wrong line. The wrong line сontains signs of the camelCase and the underscored style at same time.
     is_under = False
     is_camel = False
     while i < len(s):
-        if s[i] == '_':
-            i += 1
-            ret = ret + s[i].capitalize()
-            i += 1
+        if s[i] == '_':                     # Convert to the camelCase.
+            ret = ret + s[i+1].capitalize()
+            i += 2
             is_under = True
-            continue
-        if s[i] == s[i].capitalize():
+            
+        if s[i] == s[i].capitalize():       # Convert to the underscored style.
             ret = ret + '_' + s[i].lower()
             i += 1
             is_camel = True
-            continue
-        if s[i].isalnum():
+            
+        if s[i].isalnum():                  # Just add a simlpe letter to the output var.
             ret = ret + s[i]
             i += 1
-        if is_under and is_camel:
+        if is_under and is_camel:           # Return an error when the line is wrong.
             return('Err! The line has both styles!')
     return(ret)
 
