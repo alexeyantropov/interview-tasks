b452e29a47bb2b13a647ee0c74103c7a627dac63
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2022-10-31 18:50:57 +0300

    feat(string-to-array.py): a solution.

--- string-to-array.py
+++ string-to-array.py
@@ -1,1 +1,7 @@
-# [1.2.3.4.5] -> [1,2,3,4,5]. E.g. convert a string '1.2.3.4.5' to the array elements.
+#!/Users/aleksey.antropov/miniconda/bin/python3
+
+# ['1.2.3.4.5'] -> [1,2,3,4,5]. E.g. convert a string '1.2.3.4.5' to the array elements.
+
+input_data = ['1.2.3.4.5']
+
+print(input_data[0].split('.'))
