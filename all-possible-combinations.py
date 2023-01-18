752012b6108252402e268a6b8035f0d034cbab20
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2023-01-18 18:06:44 +0300

    initial(all-possible-combinations.py): w/o any solution

---
+++ all-possible-combinations.py
@@ -0,0 +1,1 @@
+# HackerRank: Given a string of lowercase unique characters (e.g. "ab", "lmne") sort the string alphabetically and then find all possible combinations, ignoring ordering. e.g. "ab" -> "a", "b", "ab"
