64d1474eb50fe42fc467602dc2df89ad7f48ed54
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2023-05-02 18:10:45 +0300

    fix(local-minima.py): the solution is fixed

--- local-minima.py
+++ local-minima.py
@@ -1,78 +1,34 @@
 #!/Users/aleksey.antropov/miniconda/bin/python3
 
-# Task: Find a local minima on function values
+# Task: Find a local minima for given fucntion.
+# Note https://en.wikipedia.org/wiki/Maximum_and_minimum
+
 # The test set:
-test_sets = [
-    [3,1,4,5,11,2,15,6],    # 1, 4, 2, 6
-    [7,7,4,5,11,13,15,6],   # 4, 5, 11, 6
-    [1],                    # 1
-    [2,1],                  # 1
-    [3,2,1],                # 2, 1
+graphs = [
+    [3,1,2,0,6],            # 0, 1
+    [4,2,2,1,3],            # 1
+    [1],                    # None
+    [2,1],                  # None
+    [3,2,1],                # None
     [2,1,3],                # 1
 ]
-# A function to add only uniq elemets of list:
-def add_to_arr(arr: list, val: int) -> list:
-    if val in arr:
-        return(arr)
-    else:
-        return(arr + [val])
-# The first implementaion.
-def local_minima_one(arr: list) -> list:
-    ret = []
-    if len(arr) < 2:
-        return(arr)
-    if len(arr) < 3:
-        return([min(arr)])
-    for i in range(len(arr)):
-        # Borders.
-        if (i == 0 or i == len(arr)-2) and arr[i] != arr[i+1]:
-            if arr[i] < arr[i+1]:
-                ret =  add_to_arr(ret, arr[i])
-            else:
-                ret = add_to_arr(ret, arr[i+1])
-        # The main part.
-        if i > 1 and i < len(arr)-2:
-            if arr[i-1] < arr[i] and arr[i-1] < arr[i+1]:
-                ret = add_to_arr(ret, arr[i-1])
-            if arr[i] < arr[i-1] and arr[i] < arr[i+1]:
-                ret = add_to_arr(ret, arr[i])
-            if arr[i+1] < arr[i-1] and arr[i+1] < arr[i]:
-                ret = add_to_arr(ret,arr[i+1])
-    return(ret)
-# The second implementaion, with the build-int min() finction.
-def local_minima_two(arr: list) -> list:
-    ret = []
-    if len(arr) < 2:
-        return(arr)
-    if len(arr) < 3:
-        return([min(arr)])
-    for i in range(len(arr)):
-        # Borders.
-        if (i == 0 or i == len(arr)-2) and arr[i] != arr[i+1]:
-            ret =  add_to_arr(ret, min(arr[i], arr[i+1]))
-        if i > 1 and i < len(arr)-2:
-            tmp = min(arr[i-1], arr[i], arr[i+1])
-            if arr.count(tmp) == 1:
-                ret = add_to_arr(ret, tmp)
-    return(ret)
 
-# The third one.
-def local_minima_three(arr: list) -> list:
-    ret = set()
+def local_minima(arr: list) -> list:
+    # Corner cases.
     if len(arr) < 3:
-        ret.add(min(arr))
-        return(list(ret))
-    for i in range(len(arr)):
-        if i == 0:
-            ret.add(min(arr[0:2]))
-        elif i == len(arr) - 1:
-            ret.add(min(arr[len(arr)-2:len(arr)]))
-        else:
-            ret.add(min(arr[i-1:i+2]))
+        return(None)
+    elif len(arr) == 3 and arr[0] > arr[1] < arr[2]:
+        return([arr[1]])
+    elif len(arr) == 3:
+        return(None)
+    else:
+        pass
+    # Main logic.
+    ret = set()
+    for i in range(1, len(arr) - 1):
+        if arr[i-1] > arr[i] < arr[i+1]:
+            ret.add(arr[i])
     return(list(ret))
 
-for test_set in test_sets:
-    print(local_minima_one(test_set))
-    print(local_minima_two(test_set))
-    print(local_minima_three(test_set))
-    print()
+for g in graphs:
+    print('in: {}, out: {}'.format(g, local_minima(g)))
