87e85e1148900dd5fc4be760d0bec7adbe10ffcd
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2023-06-29 14:14:36 +0300

    Revert "fix(ya-2023-01.py): the second sol is removed"
    This reverts commit 09adb4c281712a56c33d88744b9c43b3776129a0.

--- ya-2023-01.py
+++ ya-2023-01.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 import copy
-#    e=1                        e=2                   e=3
-a = [1, 5, 2, 2, 4, 2] # -> [0, 4, 1, 1, 3, 1] -> [0, 3, 0, 0, 2, 0] 
+a = [4, 5, 2, 2, 4, 2] # -> [3, 4, 1, 1, 3, 1] -> [2, 3, 0, 0, 2, 0] 
 b = a.copy()
 
+# The first solution, brute force, could be much more ops than O(N), depens from queues length.
 i, entrance = 0, 0
 while a[entrance] > 0: 
     i += 1
     entrance = i % len(a)
     a[entrance] -= 1 + 1 * i % len(a)
-    # print("{}, {}, {}".format(a, i, entrance))
+    print("{}, {}, {}".format(a, i, entrance))
 print(entrance + 1) # Human readable is needed?
+
+# The second approach, max O(2N), the 'min' and 'index' methods have O(N) complexity.
+print(b.index(min(b)) + 1)
+
